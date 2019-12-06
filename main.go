7f0e1607ccde0feeb700139f1329d3c4ba5d9fd0
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-12-06 16:51:06 +0900

    :sparkles:ADD getTime function
    ~

--- main.go
+++ main.go
@@ -5,18 +5,27 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/shurcooL/githubv4"
 	"golang.org/x/oauth2"
 )
 
+func getTime() (time.Time, time.Time) {
+	const layout = "2006-01-02T15:04:05"
+	t := time.Now()
+	y := time.Now().AddDate(0, 0, -1)
+	return t, y
+}
+
 func main() {
+	today, yesterday := getTime()
 	loadErr := godotenv.Load()
 	if loadErr != nil {
 		log.Fatal("Error loading .env file")
-
 	}
+
 	src := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
 	)
@@ -25,15 +34,20 @@ func main() {
 	client := githubv4.NewClient(httpClient)
 
 	var q struct {
-		Viewer struct {
+		User struct {
 			ContrbutionsCollection struct {
 				TotalCommitContributions githubv4.Int
-			} `graphql:"contributionsCollection(from: \"2019-12-02T00:00:00\", to: \"2019-12-03T00:00:00\")"`
-		}
+			} `graphql:"contributionsCollection(from: $Yesterday,to: $Today)"`
+		} `graphql:"user(login: \"DuGlaser\")"`
 	}
-	err := client.Query(context.Background(), &q, nil)
+	variable := map[string]interface{}{
+		"Today":     githubv4.DateTime{today},
+		"Yesterday": githubv4.DateTime{yesterday},
+	}
+
+	err := client.Query(context.Background(), &q, variable)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(q.Viewer.ContrbutionsCollection.TotalCommitContributions)
+	fmt.Println(q.User.ContrbutionsCollection.TotalCommitContributions)
 }
